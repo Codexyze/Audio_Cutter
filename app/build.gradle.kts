0e2dc97893e32fed4ea7d7247a955299bcedc8a1
Akshay Sarapure <akshaysarapure@gmail.com>
2026-03-08 15:28:45 +0530

    feat: update versionName to audio-conversion  for clarity in audio format conversion feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 14
-        versionName = "3.0"
+        versionName = "audio-conversion"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
